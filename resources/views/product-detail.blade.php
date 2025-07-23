15fa74396243351814da2eca75cfdc4f36a79c4e
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-07-23 14:06:12 +0200

    Create  ProductController and update product-detail view with correct variable name

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -1,5 +1,7 @@
 <section>
-    <h1>{{$product-name}}</h1>
+    <h1>{{$product_name}}</h1>
+    <img src="" alt="Image Fromage">
+    <p>Prix au kg</p> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pharetra maximus augue. Vivamus venenatis odio quis ligula consequat, et imperdiet nisl dignissim. Donec eu orci aliquet, hendrerit metus ac, imperdiet nulla. Vivamus venenatis odio quis ligula consequat, et imperdiet nisl dignissim. Donec eu orci aliquet, hendrerit metus ac, imperdiet nulla. </p>
     
-
 </section>

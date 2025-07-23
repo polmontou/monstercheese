5f1a9aff7ccd699d7ae252f5f2eb1cf6eb45dcc4
Paul Montoussy <paul.montoussy@gmail.com>
2025-07-23 14:03:02 +0200

    images folder added + logo and favicon inside

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\CatalogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/products',);
+Route::get('/products', [CatalogController::class, 'displayCatalog']);

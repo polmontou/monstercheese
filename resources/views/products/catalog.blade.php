26cebc1f4c3e74dde59966c90b25b9433d2aa819
Paul Montoussy <paul.montoussy@gmail.com>
2025-07-24 14:14:19 +0200

    cheese pics added, generates catalog from db

--- resources/views/products/catalog.blade.php
+++ resources/views/products/catalog.blade.php
@@ -1,44 +1,22 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalog</title>
-    <link rel="icon" type="image/x-icon" href={{ asset('images/favicon.png') }}>
-</head>
-<body>
-    @include('header')
+@extends('components.layout')
 
+@section('page_title', 'Catalogue')
+
+@section('content')
     <main>
         <h1>Nos produits</h1>
         <div class="catalogFeed">
-            <div class="catalogFeed__itemCard">
-                <img src={{ asset('images/cheese.png') }} alt="Cheese pic" class="catalogFeed_itemCard__img" width="300rem">
-                <h2 class="catalogFeed_itemCard__name">Trolleton affiné</h2> 
-                <div class="catalogFeed_itemCard__priceWeight">52,00€/kg TTC</div>
-                <div class="catalogFeed_itemCard__opinion">526 avis</div>
-                <button class="catalogFeed_itemCard__addToCart">Ajouter au panier</button>
-            </div>
-            <hr>
-            <div class="catalogFeed__itemCard">
-                <img src={{ asset('images/cheese.png') }} alt="Cheese pic" class="catalogFeed_itemCard__img" width="300rem">
-                <h2 class="catalogFeed_itemCard__name">Trolleton affiné</h2> 
-                <div class="catalogFeed_itemCard__priceWeight">52,00€/kg TTC</div>
-                <div class="catalogFeed_itemCard__opinion">526 avis</div>
-                <button class="catalogFeed_itemCard__addToCart">Ajouter au panier</button>
-            </div>
-            <hr>
-            <div class="catalogFeed__itemCard">
-                <img src={{ asset('images/cheese.png') }} alt="Cheese pic" class="catalogFeed_itemCard__img" width="300rem">
-                <h2 class="catalogFeed_itemCard__name">Trolleton affiné</h2> 
-                <div class="catalogFeed_itemCard__priceWeight">52,00€/kg TTC</div>
-                <div class="catalogFeed_itemCard__opinion">526 avis</div>
-                <button class="catalogFeed_itemCard__addToCart">Ajouter au panier</button>
-            </div>
-            <hr>
+            @foreach ($products as $product)  
+                <div class="catalogFeed__itemCard">
+                    <img src={{ asset("$product->picture") }} alt="Cheese pic" class="catalogFeed_itemCard__img" width="300rem">
+                    <h2 class="catalogFeed__itemCard__name">{{$product->name}}</h2> 
+                    <div class="catalogFeed__itemCard__price">{{$product->price}}</div>
+                    <div class="catalogFeed__itemCard__weight">{{$product->weight}}</div>
+                    <div class="catalogFeed__itemCard__opinion">526 avis</div>
+                    <a href={{ url("/product/$product->id") }}><button class="catalogFeed__itemCard__detailProduct">+ d'infos</button></a>
+                    <button class="catalogFeed__itemCard__addToCart">Ajouter au panier</button>
+                </div>
+            @endforeach
         </div>
-
-        @include('footer')
     </main>
-</body>
-</html>
+@endsection

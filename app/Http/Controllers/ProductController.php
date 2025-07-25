bd682811c0b814acebbf2a1880da5de8f88d41a0
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-07-25 10:24:33 +0200

    Enhance product-show view to display additional product details and related products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,9 @@ class ProductController extends Controller
 {
     public function show(string $productId)
     {
+        $products = DB::select('select * from products');
         $product = DB::table('products')->where('id','=', $productId)->get();
-        return view('/products.product-show', ['product' => $product]);
+        return view('/products.product-show', ['products'=>$products, 'product' => $product ]);
     }
+
 }
--- resources/views/products/product-show.blade.php
+++ resources/views/products/product-show.blade.php
@@ -1,61 +1,73 @@
 @extends('components/layout')
-@section('page_title',$product_name)
+@section('page_title',$product[0]->name)
 
 
 
 @section('content')
-    <main>
-        {{var_dump($product[0]->name)}}
-        <section>
-            <h1>{{$product[0]->name}}</h1>
-            <img src={{ asset($product[0]->picture) }} alt="Image {{$product[0]->name}}">
-            <p>Prix au kg</p>
-            <p>{{$product[0]->price}} €</p>
+<main>
+    {{var_dump($product[0]->name)}}
+    <section>
+        <h1>{{$product[0]->name}}</h1>
+        <img src={{ asset($product[0]->picture) }} alt="Image {{$product[0]->name}}">
+        <p>Prix au kg</p>
+        <p>{{$product[0]->price}} €</p>
+        <div>
+            <p>Quantité</p>
+            <button>AJouter au panier</button>
+        </div>
+        <p>{{$product[0]->description}}</p>
+        <div>
             <div>
-                <p>Quantité</p>
-                <button>AJouter au panier</button>
+                <h1>Ingrédients</h1>
+                <ul>
+                    <li>ingrédient 1</li>
+                    <li>ingrédient 2</li>
+                    <li>ingrédient 3</li>
+                </ul>
             </div>
-            <p>{{$product[0]->description}}</p>
             <div>
+                <h1>Monstre</h1>
+                <ul>
+                    <li>ingrédient 1</li>
+                    <li>ingrédient 2</li>
+                    <li>ingrédient 3</li>
+                </ul>
+            </div>
+            <div>
+                <h1>Livraison</h1>
+                <ul>
+                    <li>ingrédient 1</li>
+                    <li>ingrédient 2</li>
+                    <li>ingrédient 3</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <x-customer-review />
+        <form>
+            <textarea></textarea>
+            <button>S'abonner</button>
+        </form>
+    </section>
+
+    <section>
+        <h2>Nos autres produits</h2>
+        <div class="sectionOther">
+            @foreach ($products as $productAlone)
+            <div class="otherProducts">
+                <img src={{ asset($productAlone->picture) }}>
                 <div>
-                    <h1>Ingrédients</h1>
-                    <ul>
-                        <li>ingrédient 1</li>
-                        <li>ingrédient 2</li>
-                        <li>ingrédient 3</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1>Monstre</h1>
-                    <ul>
-                        <li>ingrédient 1</li>
-                        <li>ingrédient 2</li>
-                        <li>ingrédient 3</li>
-                    </ul>
-                </div>
-                <div>
-                    <h1>Livraison</h1>
-                    <ul>
-                        <li>ingrédient 1</li>
-                        <li>ingrédient 2</li>
-                        <li>ingrédient 3</li>
-                    </ul>
+                    <p>{{$productAlone->name}}</p>
+                    <p>{{$productAlone->price}} €</p>
+                    <a href={{ url("/product/$productAlone->id") }}>+ d'infos</a>
                 </div>
             </div>
-        </section>
-        <section>
+            @endforeach
+        </div>
+    </section>
+</main>
 
-            <x-customer-review/>
-            <form>
-                <textarea></textarea>
-                <button>S'abonner</button>
-            </form>
-        </section>
-    </main>
-
-</body>
-
-</html>
 
 @endsection
-
